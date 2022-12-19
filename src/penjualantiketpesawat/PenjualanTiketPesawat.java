65f33d0cc8abe9cf9c97cb8d922737db96469b35
Rearizth <astarothhellprince@gmail.com>
2022-12-19 13:59:40 +0700

    first commit

---
+++ src/penjualantiketpesawat/PenjualanTiketPesawat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package penjualantiketpesawat;
+
+/**
+ *
+ * @author user
+ */
+public class PenjualanTiketPesawat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        connec kon = new connec();
+        System.out.println(kon.getKoneksi());
+    }
+    
+}
